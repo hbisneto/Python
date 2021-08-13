1fa9708a5cd6f9056551c58a648f4bcbee78aacd
Heitor Bardemaker A. Bisneto <hbarbisneto@gmail.com>
2021-08-12 21:04:36 -0300

    CarConsumo

---
+++ CarConsumo/CarConsumo.py
@@ -0,0 +1,84 @@
+from time import sleep
+from datetime import date
+AnoAtual = date.today().year
+SoftwareName = "CarConsumo"
+Version = "1.3"
+CopyrightName = "Heitor Bisneto."
+print("Nome:", SoftwareName)
+print("Versão:", Version)
+print("Criado por:", CopyrightName)
+if AnoAtual == 2020:
+    print("Copyright ©", AnoAtual, "|", CopyrightName, "All rights reserved.")
+else:
+    print("Copyright © 2020 -", AnoAtual, "|", CopyrightName, "All rights reserved.")
+print("="*80)
+print(f'[{SoftwareName}] - Em Execução...')
+print("="*80)
+ProgressBar = "█"
+ProgressBarSep = "*"
+
+QtdTanque = float(input("<" + SoftwareName + "\Capacidade do Tanque> "))
+GasPreco = float(input("<" + SoftwareName + "\Preco da Gasolina por Litro> "))
+EtaPreco = float(input("<" + SoftwareName + "\Preco do Etanol por Litro> "))
+print()
+print("===============================")
+
+if GasPreco > EtaPreco:
+    MaisCaro = (GasPreco - EtaPreco)
+    Preco = (EtaPreco * QtdTanque)
+    print("<" + SoftwareName + "> Gasolina é R$", MaisCaro, "mais cara")
+    print("<" + SoftwareName + "> Necessário R$", Preco, "para completar o tanque com etanol")
+    print("===============================")
+    print()
+elif GasPreco < EtaPreco:  
+    MaisCaro = (EtaPreco - GasPreco)
+    Preco = (GasPreco * QtdTanque)
+    print("<" + SoftwareName + "> Etanol é R$", MaisCaro, "mais caro")
+    print("<" + SoftwareName + "> Necessário R$", Preco, "para completar o tanque com gasolina")
+    print("===============================")
+    print()
+else:
+    MaisCaro = (EtaPreco - GasPreco)
+    Preco = (GasPreco * QtdTanque)
+    print("<" + SoftwareName + "> Os combustíveis têm o mesmo preço. Escolha o da sua preferência")
+    print("<" + SoftwareName + "> Necessário R$", Preco, "para completar o tanque")
+    print("===============================")
+    print()
+
+Question = str(input("<" + SoftwareName + "> Gostaria de fazer os calculos de abastecimento? [Y or N]> "))
+if Question == "N" or Question == "n" or Question == "0":
+    print("O App '" + SoftwareName + "' foi encerrado corretamente...")
+elif Question == "Y" or Question == "y" or Question == "1":
+    print()
+    print("===============================")
+    print("Preparando programa de calulos")
+    print("===============================")
+    sleep(3)
+    QtdAbastecimento = float(input("<" + SoftwareName + "> Quantos litros serão abastecidos? "))
+    VeiculoConsumo = float(input("<" + SoftwareName + "> Insira o consumo do veículo por litro: "))
+    DistanciaPrevista = float(input("<" + SoftwareName + "> Insira a distância que irá percorrer: "))
+    VeiculoConsumo = (DistanciaPrevista/QtdAbastecimento)
+    print()
+    print("===============================")
+    print("<" + SoftwareName + "> O veículo consumirá em média", VeiculoConsumo, "litros por KM" )
+    print("===============================")
+
+    
+    print()
+    if QtdAbastecimento > QtdTanque:
+        print("<" + SoftwareName + ">A quantidade de abastecimento não pode ser maior que a capacidade do tanque")
+    else:
+        Abastecido = ((QtdAbastecimento / QtdTanque) * 100)
+        DispoTanqueLitros = (QtdTanque - QtdAbastecimento)
+        DispoTanque = (100 - (QtdAbastecimento / QtdTanque) * 100)
+        sleep(1)
+        print("<" + SoftwareName + "> Necessários ", DispoTanqueLitros, " litros para completar o tanque")
+        sleep(1)
+        print("<" + SoftwareName + "\Abastecimento em tanque> " + (ProgressBar * int(Abastecido)), int(Abastecido), "%")
+        sleep(1)
+        print("<" + SoftwareName + "\Espaço disponível em tanque> " + (ProgressBar * int(DispoTanque)), int(DispoTanque), "%")
+else:
+    if Question == "":
+        print("<" + SoftwareName + "\MENSAGEM>\nNenhuma opção selecionada. Execute o programa novamente.")
+    else:
+        print("<" + SoftwareName + "\MENSAGEM>\n'" + Question + "' não é uma opção válida. Execute o programa novamente.")
---
+++ CarConsumo/README.md
@@ -0,0 +1,44 @@
+# CarConsumo
+
+O programa solicita que o usuário entre com alguns valores para que sejam processadas informações de consumo de combustível. Entre essas informações:
+
+- Capacidade do tanque;
+- Preço da gasolina por litro;
+- Preço do etanol por litro;
+- Quantos litros serão abastecidos;
+- Consumo do veículo por litro;
+- Distância que o veículo irá percorrer;
+  
+
+#### Após as informações serem coletadas, o programa irá:
+
+- Comparar o valor da Gasolina com o valor do Etanol;
+- Retornar o valor total para completar o tanque do automóvel de acordo com o valor inserido;
+- Retornar o consumo médio do veículo de acordo com a distância que irá percorrer;
+- Retornar a quantidade de combustível para completar o tanque;
+- Retornar a porcentagem de abastecimento em tanque;
+- Retornar a porcentagem do espaço disponível em tanque;
+
+#
+
+#### Log de Atualizações:
+
+```
+Versão 1.1:
+- Alterações no código fonte.
+```
+```
+Versão 1.2:
+- Correções de erros
+```
+```
+Versão 1.3:
+- Adição de status de execução do programa;
+- Alteração na string do ProgressBar;
+- Alterações no padão de copyright;
+
+```
+
+#
+
+Copyright © 2020 Heitor Bisneto. All rights reserved.
