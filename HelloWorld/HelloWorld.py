d703bddd04bbdfa4f5d2068fc08856414404efaf
Heitor Bardemaker A. Bisneto <hbarbisneto@gmail.com>
2021-08-12 21:07:30 -0300

    HelloWorld

---
+++ HelloWorld/HelloWorld.py
@@ -0,0 +1,10 @@
+from datetime import date
+AnoAtual = date.today().year
+SoftwareName = "HELLO WORLD!"
+
+print("Nome:", SoftwareName)
+print("Versão: 1.1")
+print("Criado por: Heitor Bisneto")
+print("Copyright © 2019 -", AnoAtual, "Bisneto. All rights reserved.")
+print("")
+print("Hello World!")
---
+++ HelloWorld/README.md
@@ -0,0 +1,7 @@
+# Hello World!
+
+Simples "Hello World!" em Python
+
+#
+
+Copyright © 2020 Heitor Bisneto. All rights reserved.
