227da943b8c7a09d278d08faf11fc14ad69e5260
Heitor Bardemaker A. Bisneto <hbarbisneto@gmail.com>
2021-08-12 21:03:53 -0300

    AddToArray

---
+++ AddToArray/AddToArray.py
@@ -0,0 +1,79 @@
+from datetime import date
+AnoAtual = date.today().year
+SoftwareName = "AddToArray"
+Version = "2.0"
+CopyrightName = "Heitor Bisneto."
+
+print("Nome:", SoftwareName)
+print("Versão:", Version)
+print("Criado por:", CopyrightName)
+if AnoAtual == 2020:
+    print("Copyright ©", AnoAtual, "|", CopyrightName, "All rights reserved.")
+else:
+    print("Copyright © 2020 -", AnoAtual, "|", CopyrightName, "All rights reserved.")
+print("")
+
+Demo = str(input(">> Gostaria de executar uma DEMO agora? [Y/N]: "))
+if Demo == "Y" or Demo == "y" or Demo == "1":
+    Frase = "A BB CCC DDDD"
+    print("\nEscreva uma frase:",Frase)
+    WordSplit = Frase.split()
+    MyArray = []
+    Count = 0
+
+    for CurrentWord in WordSplit:
+        Count = Count + 1
+        MyArray.append(CurrentWord.upper())
+        print()
+        print("Index", Count-1,":", MyArray[Count - 1])
+        print("Linha", Count,":", MyArray)
+        
+    print()
+    print("Array:", MyArray)
+    print()
+
+    try:
+        if MyArray[0] == "A":
+            print("Execução de processo, caso o texto corresponda ao índice 0")
+            
+        if MyArray[1] == "BB":
+            print("Execução de processo, caso o texto corresponda ao índice 1")
+
+        if MyArray[2] == "CCC":
+            print("Execução de processo, caso o texto corresponda ao índice 2")
+
+        if MyArray[30] == "???":
+            print("Execução de processo, caso o texto corresponda ao índice 30")
+    except:
+        print("\n*** MENSAGEM DE TRATATIVA DE ERRO: FORA DOS ÍNDICES DA MATRIZ ***")
+elif Demo == "N" or Demo == "n" or Demo == "0":
+    Frase = str(input("> Escreva uma frase: "))
+    WordSplit = Frase.split()
+    MyArray = []
+    Count = 0
+
+    for CurrentWord in WordSplit:
+        Count = Count + 1
+        MyArray.append(CurrentWord.lower())
+        print()
+        print(">> Index", Count-1,":", MyArray[Count - 1])
+        print(">> Linha", Count,":", MyArray)
+        
+    print()
+    print(">> Array:", MyArray)
+    print()
+
+    try:
+        if MyArray[0] == "1":
+            print("> Linha 1")
+            
+        if MyArray[1] == "2":
+            print("> Linha 2")
+
+        if MyArray[2] == "3":
+            print("> Linha 3")
+
+        if MyArray[30] == "30":
+            print("> Linha 30")
+    except:
+        print("\n>>> PARECE QUE VOCÊ QUER RETORNAR UM VALOR DE UM INDEX INEXISTENTE.")
---
+++ AddToArray/README.md
@@ -0,0 +1,51 @@
+# AddToArray
+O programa ***AddToArray***, separa cada palavra de uma frase e adiciona em uma matriz.
+<br> O programa também faz o tratamento de um erro que é causado propositalmente, caso a matriz tenha menos de 30 índices.
+
+>Nesse exemplo, é possível executar uma demonstração do programa, sem que seja necessário a interação com o usuário, como mostrado no item "**AddToArray: Demo**" deste arquivo.
+
+## AddToArray: DEMO
+
+Demonstração do programa em execução
+
+```
+Nome: AddToArray
+Versão: 2.0
+Criado por: Heitor Bisneto.
+Copyright © 2020 | Heitor Bisneto. All rights reserved.
+
+Gostaria de executar uma DEMO agora? [Y/N]: Y
+
+Escreva uma frase: A BB CCC DDDD
+
+Index 0 : A
+Linha 1 : ['A']
+
+Index 1 : BB
+Linha 2 : ['A', 'BB']
+
+Index 2 : CCC
+Linha 3 : ['A', 'BB', 'CCC']
+
+Index 3 : DDDD
+Linha 4 : ['A', 'BB', 'CCC', 'DDDD']
+
+Array: ['A', 'BB', 'CCC', 'DDDD']
+
+Execução de processo, caso o texto corresponda ao índice 0
+Execução de processo, caso o texto corresponda ao índice 1
+Execução de processo, caso o texto corresponda ao índice 2
+
+*** MENSAGEM DE TRATATIVA DE ERRO: FORA DOS ÍNDICES DA MATRIZ ***
+```
+#### Log de Atualização:
+
+```
+Versão 2.0:
+- Mudança de nome para "AddToArray"
+- Pequenas alterações de textos do programa;
+```
+
+#
+
+Copyright © 2020 Heitor Bisneto. All rights reserved.
